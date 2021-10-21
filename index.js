34371c5a9f4b5a9b2aa404031d546e47c8f797c8
Michael Hayes <michael.hayes@thetradedesk.com>
2021-10-20 18:30:49 -0700

    Add small delay on button push

--- index.js
+++ index.js
@@ -2,47 +2,49 @@ const rpio = require("rpio");
 
 rpio.init({
   gpiomem: true,
-  mapping: 'gpio',
+  mapping: "gpio",
   close_on_exit: true,
-})
-
+});
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
 
 function debounce(fn, ms) {
   let timer;
 
   return (...args) => {
-      if (timer) {
-        clearTimeout(timer)
-      }
-      timer = setTimeout(() => {
-        timer = null;
-        
-        return fn(...args);
-      }, ms)
-  }
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      timer = null;
+
+      return fn(...args);
+    }, ms);
+  };
 }
 
 async function waitForButton(pin) {
   rpio.open(pin, rpio.INPUT, rpio.PULL_UP);
-  console.log('waiting for button')
+  console.log("waiting for button");
 
   return new Promise((resolve) => {
-    rpio.poll(pin, () => {
-      console.log('button pressed')
-      if (rpio.read(pin)) {
-        return
-      }
-              
-      rpio.poll(pin, null)
-      rpio.close(pin)
-      resolve()
-    }, rpio.POLL_LOW)
-  })
+    rpio.poll(
+      pin,
+      () => {
+        rpio.msleep(50);
+        if (rpio.read(pin)) {
+          return;
+        }
+
+        rpio.poll(pin, null);
+        rpio.close(pin);
+        resolve();
+      },
+      rpio.POLL_LOW
+    );
+  });
 }
 
 const command = {
@@ -76,15 +78,15 @@ const command = {
 };
 
 async function runCommand({ steps }) {
-  for (const {pins, duration } of steps) {
+  for (const { pins, duration } of steps) {
     for (const pin of pins) {
-      rpio.open(pin.pin, pin.mode, pin.level)
+      rpio.open(pin.pin, pin.mode, pin.level);
     }
 
-    await sleep(duration)
+    await sleep(duration);
 
     for (const pin of pins) {
-      rpio.close(pin.pin)
+      rpio.close(pin.pin);
     }
   }
 }
@@ -92,8 +94,10 @@ async function runCommand({ steps }) {
 async function main() {
   while (true) {
     await waitForButton(21);
+    await sleep(500)
+    console.log('running command')
     await runCommand(command);
   }
 }
 
-main().then(() => console.log('done'))
+main().then(() => console.log("done"));
